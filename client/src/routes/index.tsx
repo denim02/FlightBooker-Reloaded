7b9226fc239db7d0c8342801433d1af918e16bf0
Deni <denimastori02@gmail.com>
2025-01-04 13:26:20 +0100

    [feat] include main packages in client app

---
+++ client/src/routes/index.tsx
@@ -0,0 +1,17 @@
+import api from '@/utils/api'
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({
+  component: Index,
+  loader: () => api.get('/'),
+})
+
+function Index() {
+  const data = Route.useLoaderData()
+  return (
+    <div className="p-2">
+      <h3>Welcome Home!</h3>
+      {data && <p>{data.data['Laravel']}</p>}
+    </div>
+  )
+}
---
+++ client/src/utils/api.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const createAxiosInstance = () => {
+  return axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+
+const api = createAxiosInstance();
+
+export default api;
